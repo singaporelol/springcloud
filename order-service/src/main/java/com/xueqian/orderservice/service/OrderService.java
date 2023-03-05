cc3516acb1f27aa69e4a37a62e3f9c1fb6207f86
weiji <weiji@DESKTOP-ND2J4PP>
2023-03-05 13:12:18 +0800

    添加并使用Feign，使用注解和yaml文件配置Feign的日志级别，注释掉了注解功能

--- order-service/src/main/java/com/xueqian/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/xueqian/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.xueqian.orderservice.service;
 
+import com.xueqian.orderservice.FeignClientDemo.UserClient;
 import com.xueqian.orderservice.mapper.OrderMapper;
 import com.xueqian.orderservice.pojo.Order;
 import com.xueqian.orderservice.pojo.User;
@@ -13,16 +14,39 @@ public class OrderService {
     private OrderMapper orderMapper;
     @Resource
     private RestTemplate restTemplate;
+    //使用Feign，拿到容器内的Userclient对象
+    @Resource
+    private UserClient userClient;
 
+    /**
+     * 使用Feign调用，替代restTemplate(不优雅)
+     * @param orderId
+     * @return
+     */
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-//        String url="http://localhost:8081/user/" +order.getUserId();
-        String url="http://userservice/user/" +order.getUserId();
-
-        User user = restTemplate.getForObject(url, User.class);
+        // String url="http://userservice/user/" +order.getUserId();
+        // User user = restTemplate.getForObject(url, User.class);
+        //2.利用Feign发起http请求，查询用户
+        User user = userClient.findById(order.getUserId());
         order.setUser(user);
         // 4.返回
         return order;
     }
+
+    /*
+    * 使用nacos，eureka调用下面的方法
+    * */
+//     public Order queryOrderById(Long orderId) {
+//         // 1.查询订单
+//         Order order = orderMapper.findById(orderId);
+// //        String url="http://localhost:8081/user/" +order.getUserId();
+//         String url="http://userservice/user/" +order.getUserId();
+
+//         User user = restTemplate.getForObject(url, User.class);
+//         order.setUser(user);
+//         // 4.返回
+//         return order;
+//     }
 }
